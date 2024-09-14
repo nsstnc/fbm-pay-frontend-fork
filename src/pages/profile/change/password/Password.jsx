8f04c7bf3c4044018690de5f566a1f0065c19b6a
wwwclay <nurgazya94@gmail.com>
2024-09-14 15:17:59 +0600

    update

--- src/pages/profile/change/password/Password.jsx
+++ src/pages/profile/change/password/Password.jsx
@@ -1,49 +1,61 @@
-import style from '../profile/profile.module.css'
-import appStyle from '../../../../app.module.css'
-import Menu from "../../../../generic/menu/Menu.jsx";
-import Field from "../../settings/field/Field.jsx";
-import {
-  PROFILE_SETTINGS_ROUTE,
-} from "../../../../routing/consts.js";
-import Input from "../../../userManagement/input/Input.jsx";
-import Button from "../../../../generic/buttons/Button.jsx";
-import WhiteButton from "../../../../generic/buttons/WhiteButton.jsx";
+import style from "../profile/profile.module.css"
+import appStyle from "../../../../app.module.css"
+import Menu from "../../../../generic/menu/Menu.jsx"
+import Field from "../../settings/field/Field.jsx"
+import { PROFILE_SETTINGS_ROUTE } from "../../../../routing/consts.js"
+import Input from "../../../userManagement/input/Input.jsx"
+import Button from "../../../../generic/buttons/Button.jsx"
+import WhiteButton from "../../../../generic/buttons/WhiteButton.jsx"
+import { useValue } from "../../../../generic/model/value.jsx"
+import { $isMenuOpened } from "../../../../generic/model/menuModel.js"
 
 const Password = () => {
+  //hanldeMenu
+  const [opened] = useValue($isMenuOpened)
+
   return (
     <div className={appStyle.page}>
-      <Menu/>
+      <Menu />
 
-      <div className={appStyle.main}>
+      <div className={`${appStyle.main} ${opened ? appStyle.active : ""}`}>
         <h1 className={appStyle.pageTitle}>Change password</h1>
 
         <div className={style.pageContent}>
-          <Field title="New password" value={
-            <div className={style.input}>
-              <Input hint="Type here a new pass"/>
-            </div>
-          }/>
-
-          <Field title="Confirm password" value={
-            <div className={style.input}>
-              <Input hint="Type it again"/>
-            </div>
-          }/>
-
-          <Field title="E-mail 2FA" value={
-            <div className={style.andButton}>
-              <div className={style.input1}>
-                <Input hint="Enter the 2FA code sent to your email"/>
+          <Field
+            title="New password"
+            value={
+              <div className={style.input}>
+                <Input hint="Type here a new pass" />
               </div>
-              <div className={style.Button}>
-                <Button text="Request code"/>
+            }
+          />
+
+          <Field
+            title="Confirm password"
+            value={
+              <div className={style.input}>
+                <Input hint="Type it again" />
               </div>
-            </div>
-          }/>
+            }
+          />
+
+          <Field
+            title="E-mail 2FA"
+            value={
+              <div className={style.andButton}>
+                <div className={style.input1}>
+                  <Input hint="Enter the 2FA code sent to your email" />
+                </div>
+                <div className={style.Button}>
+                  <Button text="Request code" />
+                </div>
+              </div>
+            }
+          />
 
           <div className={style.buttons}>
-            <Button text="Change" link={PROFILE_SETTINGS_ROUTE}/>
-            <WhiteButton text="Cancel" link={PROFILE_SETTINGS_ROUTE}/>
+            <Button text="Change" link={PROFILE_SETTINGS_ROUTE} />
+            <WhiteButton text="Cancel" link={PROFILE_SETTINGS_ROUTE} />
           </div>
         </div>
       </div>
